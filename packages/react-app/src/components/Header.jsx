1fb038a514e2641b3612a29e60d354a0695f3227
@askcasmir <casmirconsults@gmail.com>
2023-06-09 19:02:20 -0500

    added counter for chainlinks upkeep cron job

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
   return (
     <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="forkable Ethereum dev stack focused on fast product iteration"
+        title="Time ⌚️ Keeper"
+        subTitle="chain.link function keeper"
         style={{ cursor: "pointer" }}
       />
     </a>
